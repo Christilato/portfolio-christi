2d6beeafb2234bffb373b59b2a7c5ce3b192d20d
Christilato <christilato@gmail.com>
2022-12-03 10:06:44 -0600

    contact card

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -76,67 +76,72 @@ function Contact() {
                 method='POST'
                 encType='multipart/form-data'
               >
-                <div className="gird md:grid-cols-2 gap-4 w-full py-2">
-                  <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">Name</label>
+                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2 text-white'>Name</label>
                     <input
-                      className="border-2 rounded-lg p-3 flex text-black border-gray-300"
-                      type="text"
+                      className='border-2 rounded-lg p-3 flex border-gray-300 text-black'
+                      type='text'
+                      name='name'
                     />
                   </div>
-                  <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2 text-white'>
                       Phone Number
                     </label>
                     <input
-                      className="border-2 rounded-lg p-3 flex text-black border-gray-300"
-                      type="text"
+                      className='border-2 rounded-lg p-3 flex border-gray-300 text-black'
+                      type='text'
+                      name='phone'
                     />
                   </div>
                 </div>
-
-                <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Email</label>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2 text-white'>Email</label>
                   <input
-                    className="border-2 rounded-lg p-3 flex text-black border-gray-300"
-                    type="email"
+                    className='border-2 rounded-lg p-3 flex border-gray-300 text-black'
+                    type='email'
+                    name='email'
                   />
                 </div>
-
-                <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Subject</label>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2 text-white'>Subject</label>
                   <input
-                    className="border-2 rounded-lg p-3 flex text-black border-gray-300"
-                    type="text"
+                    className='border-2 rounded-lg p-3 flex border-gray-300 text-black'
+                    type='text'
+                    name='subject'
                   />
                 </div>
-
-                <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Message</label>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2 text-white'>Message</label>
                   <textarea
-                    className="border-2 rounded-lg p-3 text-black border-gray-300"
-                    rows="10"
-                  />
+                    className='border-2 rounded-lg p-3 border-gray-300 text-black'
+                    rows='10'
+                    name='message'
+                  ></textarea>
                 </div>
-                <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
+                <button className='w-full p-4 text-gray-100 mt-4'>
+                  Send Message
+                </button>
               </form>
             </div>
           </div>
         </div>
         <div className='flex justify-center py-12'>
-            <Link href='/'>
-                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+          <Link href='/'>
+         
+              <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <HiOutlineChevronDoubleUp
                   className='text-[#92425d]'
                   size={30}
                 />
-                </div>
-            </Link>
+              </div>
+        
+          </Link>
         </div>
-
       </div>
     </div>
   );
-}
+};
 
 export default Contact;
